3db73a6da0f553664f649b44c26d7f81063d19e4
Prateek Kumar <Prateek.Kumar@synapse.com>
2020-09-14 19:20:33 +0530

    Initial Commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { IonPage, IonContent } from '@ionic/react';
+
+const HomePage: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent>
+        <h2>Hello World</h2>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default HomePage;
